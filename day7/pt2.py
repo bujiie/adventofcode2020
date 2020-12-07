d4ae21f928e4f83d937b300236aafbcbddd8d5db
brian.ujiie <brian.ujiie@workday.com>
2020-12-07 10:00:25 -0800

    Day7 cleanup.

--- day7/pt2.py
+++ day7/pt2.py
@@ -12,32 +12,31 @@ bags_cnt = {}
 with open(filename) as fp:
     for index, line in enumerate(fp):
         clean_line = line.replace('.', '').strip()
-
-        (carrier, contents) = clean_line.split('contain')
-        color = carrier.replace('bags', '').strip()
-        bags[color] = []
-        bags_cnt[color] = 0
+        (outer_bag, inner_bags) = clean_line.split('contain')
+        color = outer_bag.replace('bags', '').strip()
+        bags[color] = {'total': 0, 'bags': []}
 
         # move to next rule if no bags can be placed inside
-        if contents.strip() != 'no other bags':
-            for content in contents.split(','):
-                results = re.search('^(\d+)(.*)$', content.strip())
-                qty = results[1].strip()
-                bag_color = re.sub(r'bags?', '', results[2].strip()).strip()
-                bags[color].append({'color': bag_color, 'qty': int(qty)})
-                bags_cnt[color] = bags_cnt[color] + int(qty)
+        if inner_bags.strip() == 'no other bags':
+            continue
+
+        for content in inner_bags.split(','):
+            matches = re.search('^(\d+)(.*)$', content.strip())
+            qty = int(matches[1].strip())
+            bag_color = re.sub(r'bags?', '', matches[2].strip()).strip()
+
+            bags[color]['bags'].append({'color': bag_color, 'qty': qty})
+            bags[color]['total'] += qty
+
 
 target = 'shiny gold'
-
-
 queue = [{'color': target, 'qty': 1}]
 count = 0
-
 while queue:
     node = queue.pop(0)
-    count = count + node['qty'] * bags_cnt[node['color']]
+    count = count + node['qty'] * bags[node['color']]['total']
 
-    for inner_bag in bags[node['color']]:
+    for inner_bag in bags[node['color']]['bags']:
         queue.append({'color': inner_bag['color'], 'qty':  node['qty'] * inner_bag['qty']})
 
 print(count)
